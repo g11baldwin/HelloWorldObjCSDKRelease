5256a1e22bf6ff50aa27cdb2e6e5c303d4359328
Unknown <dev@dynepic.com>
2018-03-30 07:59:35 -0400

    now supports anon. user & global storage

--- HelloWorld/AppDelegate.m
+++ HelloWorld/AppDelegate.m
@@ -18,17 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
 	//Replace these with the values your generated on the playPORTAL Partner Dashboard
-
-
-
-// Test server (fast token expiration
-//    NSString *cid = @"iok-cid-5aa9777a4a7923664a99c195b10547c212829efc877c57c1";
-//    NSString *cse = @"iok-cse-4ff4fe6a59654f330c6a02d7eb29d43965efa95a82df2311";
-
-    // Sandbox cid / cse
-    // For testing use: redfish573/HelloWorld
-        NSString *cid = @"iok-cid-e1fa99fb361123b78cb6bcafcd639fb49c31766bc3eddf94";
-        NSString *cse = @"iok-cse-1778988ca3843a3c227c3fa5ced3be62d359a0aa87a169d5";
+    NSString *cid = @"YOUR_CLIENT_ID_HERE";
+    NSString *cse = @"YOUR_CLIENT_SECRET_HERE";
 
     
     NSString *redirectURI = @"helloworld://redirect";
